88a4b73124317a1401f8cea41ab7785aca0f9a62
MackorLab <76862637+MackorLab@users.noreply.github.com>
2022-12-20 19:48:08 +0700

    Update renderProducts.js

--- js/renderProducts.js
+++ js/renderProducts.js
@@ -1,1 +1,129 @@
-!function(t){var n={};function e(o){if(n[o])return n[o].exports;var c=n[o]={i:o,l:!1,exports:{}};return t[o].call(c.exports,c,c.exports,e),c.l=!0,c.exports}e.m=t,e.c=n,e.d=function(t,n,o){e.o(t,n)||Object.defineProperty(t,n,{enumerable:!0,get:o})},e.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},e.t=function(t,n){if(1&n&&(t=e(t)),8&n)return t;if(4&n&&"object"==typeof t&&t&&t.__esModule)return t;var o=Object.create(null);if(e.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:t}),2&n&&"string"!=typeof t)for(var c in t)e.d(o,c,function(n){return t[n]}.bind(null,c));return o},e.n=function(t){var n=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(n,"a",n),n},e.o=function(t,n){return Object.prototype.hasOwnProperty.call(t,n)},e.p="",e(e.s=0)}([function(t,n){const e=document.querySelector("#products-container");function o(t){t.forEach((function(t){const n=`<div class="col-md-6">\n\t\t\t\t\t\t<div class="card mb-4" data-id="${t.id}">\n\t\t\t\t\t\t\t<img class="product-img" src="img/roll/${t.imgSrc}" alt="">\n\t\t\t\t\t\t\t<div class="card-body text-center">\n\t\t\t\t\t\t\t\t<h4 class="item-title">${t.title}</h4>\n\t\t\t\t\t\t\t\t<p><small data-items-in-box class="text-muted">${t.itemsInBox} шт.</small></p>\n\n\t\t\t\t\t\t\t\t<div class="details-wrapper">\n\n\t\t\t\t\t\t\t\t\t\x3c!-- Счетчик --\x3e\n\t\t\t\t\t\t\t\t\t<div class="items counter-wrapper">\n\t\t\t\t\t\t\t\t\t\t<div class="items__control" data-action="minus">-</div>\n\t\t\t\t\t\t\t\t\t\t<div class="items__current" data-counter>1</div>\n\t\t\t\t\t\t\t\t\t\t<div class="items__control" data-action="plus">+</div>\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t\t\x3c!-- // Счетчик --\x3e\n\n\t\t\t\t\t\t\t\t\t<div class="price">\n\t\t\t\t\t\t\t\t\t\t<div class="price__weight">${t.weight}г.</div>\n\t\t\t\t\t\t\t\t\t\t<div class="price__currency">${t.price} ₽</div>\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t</div>\n\n\t\t\t\t\t\t\t\t<button data-cart type="button" class="btn btn-block btn-outline-warning">\n\t\t\t\t\t\t\t\t\t+ в корзину\n\t\t\t\t\t\t\t\t</button>\n\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>`;e.insertAdjacentHTML("beforeend",n)}))}!async function(){const t=await fetch("./js/products_obs.json");o(await t.json())}(),document.querySelector("#rolss").onclick=function(){!async function(){const t=await fetch("./js/products_roll.json"),n=await t.json();e.innerHTML="",o(n),console.log("ROLL")}()},document.querySelector("#sushs").onclick=function(){!async function(){const t=await fetch("./js/products_sushi.json"),n=await t.json();e.innerHTML="",o(n),console.log("SUSHI")}()},document.querySelector("#sups").onclick=function(){!async function(){const t=await fetch("./js/products_sup.json"),n=await t.json();e.innerHTML="",o(n),console.log("SUP")}()}}]);
+const productsContainer = document.querySelector('#products-container');
+
+// Запускаем getProducts
+
+
+// Асинхронная функция получения данных из файла products.json
+async function getProducts_obs() {
+	// Получаем данные из products.json
+	const response = await fetch('./js/products_obs.json');
+	// Парсим данные из JSON формата в JS
+	const productsArray = await response.json();
+	// Запускаем ф-ю рендера (отображения товаров)
+	renderProducts(productsArray);
+};
+getProducts_obs();
+
+
+
+
+async function getProducts_roll() {
+	// Получаем данные из products.json
+	const response = await fetch('./js/products_roll.json');
+	// Парсим данные из JSON формата в JS
+	const productsArray = await response.json();
+	// Запускаем ф-ю рендера (отображения товаров)
+    
+
+	productsContainer.innerHTML = "";
+
+
+	renderProducts(productsArray);
+	console.log("ROLL")
+};
+document.querySelector("#rolss").onclick = function(){
+	getProducts_roll() ;
+	
+  };
+
+
+
+
+
+  async function getProducts_sushi() {
+	// Получаем данные из products.json
+	const response = await fetch('./js/products_sushi.json');
+	// Парсим данные из JSON формата в JS
+	const productsArray = await response.json();
+	// Запускаем ф-ю рендера (отображения товаров)
+	
+
+	productsContainer.innerHTML = "";
+
+
+
+	renderProducts(productsArray);
+	console.log("SUSHI")
+};
+
+  document.querySelector("#sushs").onclick = function(){
+	getProducts_sushi();
+	
+  };
+
+
+
+
+
+  async function getProducts_sup() {
+	// Получаем данные из products.json
+	const response = await fetch('./js/products_sup.json');
+	// Парсим данные из JSON формата в JS
+	const productsArray = await response.json();
+	// Запускаем ф-ю рендера (отображения товаров)
+	
+
+    productsContainer.innerHTML = "";
+
+
+
+	renderProducts(productsArray);
+	console.log("SUP")
+};
+ document.querySelector("#sups").onclick = function(){
+	getProducts_sup();
+	
+ };
+
+
+
+
+
+
+
+
+function renderProducts(productsArray) {
+    productsArray.forEach(function (item) {
+        const productHTML = `<div class="col-md-6">
+						<div class="card mb-4" data-id="${item.id}">
+							<img class="product-img" src="img/roll/${item.imgSrc}" alt="">
+							<div class="card-body text-center">
+								<h4 class="item-title">${item.title}</h4>
+								<p><small data-items-in-box class="text-muted">${item.itemsInBox} шт.</small></p>
+
+								<div class="details-wrapper">
+
+									<!-- Счетчик -->
+									<div class="items counter-wrapper">
+										<div class="items__control" data-action="minus">-</div>
+										<div class="items__current" data-counter>1</div>
+										<div class="items__control" data-action="plus">+</div>
+									</div>
+									<!-- // Счетчик -->
+
+									<div class="price">
+										<div class="price__weight">${item.weight}г.</div>
+										<div class="price__currency">${item.price} ₽</div>
+									</div>
+								</div>
+
+								<button data-cart type="button" class="btn btn-block btn-outline-warning">
+									+ в корзину
+								</button>
+
+							</div>
+						</div>
+					</div>`;
+        productsContainer.insertAdjacentHTML('beforeend', productHTML);
+    });
+}
